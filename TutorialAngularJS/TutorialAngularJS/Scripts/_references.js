71fbc1ec12c189a1246c9e3ccf186f379274f5aa
Alejandro Diaz Betancourt <alejodb@hotmail.com>
2015-09-13 22:40:29 -0500

    Filtros

--- TutorialAngularJS/TutorialAngularJS/Scripts/_references.js
+++ TutorialAngularJS/TutorialAngularJS/Scripts/_references.js
@@ -2,5 +2,6 @@
 /// <reference path="angular.js" />
 /// <reference path="angular-local-storage.js" />
 /// <reference path="angular-mocks.js" />
+/// <reference path="filtros_controller.js" />
 /// <reference path="primer_controller.js" />
 /// <reference path="todolist_controller.js" />
---
+++ TutorialAngularJS/TutorialAngularJS/Scripts/filtros_controller.js
@@ -0,0 +1,13 @@
+﻿angular.module("ModuloProbarFiltros", [])
+    .filter("miFiltroQuitarHtml", function () {
+        return function (texto) {
+            return String(texto).replace(/<[^>]+>/gm, '');
+        }
+    })
+    .controller("FiltrosController", function($scope){
+        $scope.mihtml = "<p>Hola mundo</p>"
+        $scope.objetoprueba = {};
+        $scope.objetoprueba.titulo = "Saludo";
+        $scope.objetoprueba.descripcion = "Hola Mundo";
+        $scope.valor = 2;
+    });
