f88870c25585ab64e2230ea1831425f23e96fda0
Alejandro Diaz Betancourt <alejodb@hotmail.com>
2015-09-26 07:54:44 -0500

    Route

---
+++ TutorialAngularJS/TutorialAngularJS/route/app.js
@@ -0,0 +1,13 @@
+﻿angular.module("CustomDirective", ["ngRoute"])
+.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            controller: "RepositoriosController",
+            templateUrl: "templates/home.html"
+        })
+        .when("/repositorio/:name", {
+            controller: "RepositorioController",
+            templateUrl: "templates/repositorio.html"
+        })
+        .otherwise("/");
+});
---
+++ TutorialAngularJS/TutorialAngularJS/route/controllers.js
@@ -0,0 +1,35 @@
+﻿angular.module("CustomDirective")
+.controller("RepositoriosController", function ($scope, $http) {
+    $scope.listaAutocompletar = [];
+    $scope.repositorios = [];
+
+    $http.get("https://api.github.com/users/twitter/repos")
+    .success(function (data) {
+        $scope.repositorios = data;
+
+        for (var i = data.length - 1; i >= 0; i--) {
+            var repositorio = data[i];
+            $scope.listaAutocompletar.push(repositorio.name);
+        };
+    })
+    .error(function (err) {
+        console.log(err);
+    });
+
+    $scope.optionSelected = function (data) {
+        $scope.$apply(function () {
+            $scope.repositorioSeleccionado = data;
+        })
+    }
+})
+.controller("RepositorioController", function ($scope, $http, $routeParams) {
+    $scope.repositorio = {};
+
+    $http.get("https://api.github.com/repos/twitter/"+$routeParams.name)
+    .success(function (data) {
+        $scope.repositorio = data;
+    })
+    .error(function (err) {
+        console.log(err);
+    });
+})
