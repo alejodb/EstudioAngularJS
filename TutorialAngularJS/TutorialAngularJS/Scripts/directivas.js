73fd6d8f6fcd9232b8ca0ac683504d237f8c1b88
Alejandro Diaz Betancourt <alejodb@hotmail.com>
2015-09-25 21:58:40 -0500

    Directivas parte 2

--- TutorialAngularJS/TutorialAngularJS/Scripts/directivas.js
+++ TutorialAngularJS/TutorialAngularJS/Scripts/directivas.js
@@ -1,4 +1,24 @@
 ﻿angular.module("CustomDirective", [])
+.directive("myAutocomplete", function () {
+    function link(scope, element, attrs) {
+        $(element).autocomplete({
+            source: scope[attrs.myAutocomplete],
+            select: function (ev, ui) {
+                ev.preventDefault();
+                if (ui.item) {
+                    scope.optionSelected(ui.item.value);
+                }
+            },
+            focus: function (ev, ui) {
+                ev.preventDefault();
+                $(this).val(ui.item.label);
+            }
+        });
+    };
+    return {
+        link: link
+    };
+})
 .directive('backImg', function () {
     return function(scope, element, attrs) {
         attrs.$observe('backImg', function(value) { // esto porque el valor a observar esta en {{}} de angular
@@ -11,11 +31,25 @@
     }
 })
 .controller("AppCtrl", function ($scope, $http) {
+    $scope.listaAutocompletar = [];
+    $scope.repositorios = [];
+
     $http.get("https://api.github.com/users/alejodb/repos")
     .success(function (data) {
-        $scope.repos = data;
+        $scope.repositorios = data;
+
+        for (var i = data.length - 1; i >= 0; i--) {
+            var repositorio = data[i];
+            $scope.listaAutocompletar.push(repositorio.name);
+        };
     })
     .error(function (err) {
         console.log(err);
     });
+
+    $scope.optionSelected = function (data) {
+        $scope.$apply(function() {
+            $scope.repositorioSeleccionado = data;
+        })
+    }
 });
